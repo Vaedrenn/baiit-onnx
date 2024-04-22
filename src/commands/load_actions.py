d517941adbc5adeb8858a81d47a63c39d51285c6
vaedrenn <brianto10622@gmail.com>
2024-04-22 16:54:07 -0400

    labels works

--- src/commands/load_actions.py
+++ src/commands/load_actions.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
 import os
+from typing import Type
+
+import pandas as pd
+import numpy as np
 
 import onnxruntime as rt
 from onnxruntime import InferenceSession
@@ -27,3 +31,34 @@ def load_model(model_path: str | os.path) -> InferenceSession | None:
     print("Finished Loading models")
     return model
 
+
+def load_labels(model_path: str | os.path, filename: str, categories: dict) -> dict:
+    """
+    Loads labels from  txt file
+    :param filename:name of label file
+    :param categories: cat name : cat number
+    :param model_path: file name
+    :return: list of tags(labels)
+    """
+    tag_path = os.path.join(model_path, filename)
+    print(tag_path)
+
+    if not os.path.exists(tag_path):
+        # Default path name
+        tag_path = os.path.join(model_path, "selected_tags.csv")
+
+    labels = {}
+    try:
+        tags_df = pd.read_csv(tag_path)
+        tags = tags_df["name"]
+        tag_names = tags.tolist()
+
+        for category_name, category_number in categories.items():
+            index = list(np.where(tags_df["category"] == category_number)[0])
+            labels [category_name] = index
+
+    except (FileNotFoundError, IOError, OSError, PermissionError) as file_error:
+        print(f"Error reading labels file: {file_error}")
+        return {}
+
+    return labels
