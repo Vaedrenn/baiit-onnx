80888e4e18387c6ae8d160fa8d689b5d5f9b495f
vaedrenn <brianto10622@gmail.com>
2024-04-26 12:42:00 -0400

    split off preprocessing of images for clarity

--- src/commands/predict_all.py
+++ src/commands/predict_all.py
@@ -1,90 +1,15 @@
 from __future__ import annotations
 
 import os
-from collections import OrderedDict
-from typing import Any, Dict, List, Tuple
-import onnxruntime as rt
+from typing import Any
+
 import numpy as np
+import onnxruntime
+import onnxruntime as rt
 from PIL import Image
-from PyQt5.QtCore import QRunnable, QThreadPool
+
 from src.commands.load_actions import load_labels, load_model
-
-
-class Runnable(QRunnable):
-    """
-    Preprocesses images from directory using qt's multiprocessing model
-    :param image_path: file name
-    :param size: dimensions to resize to
-    :param preprocessed_images: return array
-    """
-
-    def __init__(self, image_path, size, preprocessed_images):
-
-        super().__init__()
-        self.image_path = image_path
-        self.size = size
-        self.preprocessed_images = preprocessed_images
-
-    def run(self):
-        try:
-            # Model only supports 3 channels
-            image = Image.open(self.image_path).convert('RGB')
-
-            # Pad image to square
-            image_shape = image.size
-            max_dim = max(image_shape)
-            pad_left = (max_dim - image_shape[0]) // 2
-            pad_top = (max_dim - image_shape[1]) // 2
-
-            padded_image = Image.new("RGB", (max_dim, max_dim), (255, 255, 255))
-            padded_image.paste(image, (pad_left, pad_top))
-
-            # Resize
-            if max_dim != self.size:
-                padded_image = padded_image.resize(
-                    self.size,
-                    Image.LANCZOS,
-                )
-
-            # Convert to numpy array
-            image_array = np.asarray(padded_image, dtype=np.float32)
-
-            # Convert PIL-native RGB to BGR
-            image_array = image_array[:, :, ::-1]
-
-            image_array = np.expand_dims(image_array, axis=0)
-
-            # shape needs to be 4 dims
-
-            self.preprocessed_images.append((self.image_path, image_array))
-
-        except Exception as e:
-            print(f"Runnable Error processing {self.image_path}: {e}")
-
-
-def process_images_from_directory(model: rt.InferenceSession, directory: str | os.path) -> list[(str, np.ndarray)]:
-    """
-    Processes all images in a directory, does not go into subdirectories.
-    Images need to be shaped before predict can be called on it.
-    :param model: model, shape is used to resize of images
-    :param directory: directory of images to be precessed
-    :return: [(filename, ndarray)] returns a list of file names and processed images
-    """
-    preprocessed_images = []
-    image_filenames = os.listdir(directory)
-    pool = QThreadPool.globalInstance()
-
-    # get dimensions from model
-    _, height, width, _ = model.get_inputs()[0].shape
-    size = (height, width)
-
-    for filename in image_filenames:
-        image_path = os.path.join(directory, filename)
-        runnable = Runnable(image_path, size, preprocessed_images)
-        pool.start(runnable)
-
-    pool.waitForDone()
-    return preprocessed_images
+from src.commands.process_images import process_images_from_directory
 
 
 def predict(
@@ -145,9 +70,10 @@ def predict_all(model: rt.InferenceSession,
     :param labels: {"category":[indexes], "category":[2,3,4,5]}
     :param directory: folder to process
     :return: None if there are no tags within threshold otherwise returns:
-
     """
+
     images = process_images_from_directory(model, directory)
+
     processed_images = []
     for image in images:
         result = predict(model, labels, thresholds, image[1])
@@ -158,6 +84,10 @@ def predict_all(model: rt.InferenceSession,
     else:
         print("No results")
         return None
+
+
+def predict_parallel(model, labels, thresholds, images):
+    pass
 
 
 def test_predict():
@@ -211,7 +141,6 @@ def test_predict_all():
     thresh_dict = {"rating": 0.0, "general": 0.5, "characters": 0.85}
     labels = load_labels(path, "selected_tags.csv", test_dict)
     img_path = r"C:\Users\khei\PycharmProjects\baiit-onnx\tests\images"
-    img_path = r"C:\Users\khei\Pictures\x0x0x"
 
     results = predict_all(model, labels, thresh_dict, img_path)
     for r in results:
